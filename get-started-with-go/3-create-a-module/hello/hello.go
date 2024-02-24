ce0dbadc457ddc1443edcb7713c404b13c3ca6fb
kairavkkp <kairavpithadia13@gmail.com>
2024-02-24 11:31:27 +0530

    feat: map and for loops

--- get-started-with-go/3-create-a-module/hello/hello.go
+++ get-started-with-go/3-create-a-module/hello/hello.go
@@ -11,9 +11,13 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Kairav")
+	names := []string{
+		"Kairav", "Ketan", "Vaishali",
+	}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
