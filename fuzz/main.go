952d6b60defe4f05b34dbabce3fadb0b15ed1e15
kairavkkp <kairavpithadia13@gmail.com>
2024-02-24 16:20:44 +0530

    feat: fuzzing tests

---
+++ fuzz/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
+
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is not valid UTF-8")
+	}
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
+}
+
+func main() {
+	input := "This is me testing fuzz"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("rev: %q, err: %q\n", rev, revErr)
+	fmt.Printf("doubleRev: %q, err: %q\n", doubleRev, doubleRevErr)
+}
---
+++ fuzz/reverse_test.go
@@ -0,0 +1,44 @@
+package main
+
+import "testing"
+import "unicode/utf8"
+
+func TestReverse(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{" ", " "},
+		{"!12345", "54321!"},
+	}
+	for _, tc := range testcases {
+		rev, _ := Reverse(tc.in)
+		if rev != tc.want {
+			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+		}
+	}
+}
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, revErr := Reverse(orig)
+		if revErr != nil {
+			return
+		}
+		doubleRev, doubleRevErr := Reverse(rev)
+		if doubleRevErr != nil {
+			return
+		}
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		if doubleRev != orig {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
+}
