6259a9ab5f9f6fcfe027c1b6850559c662db8758
kairavkkp <kairavpithadia13@gmail.com>
2024-02-24 11:25:12 +0530

    feat: slices

--- get-started-with-go/3-create-a-module/greetings/greetings.go
+++ get-started-with-go/3-create-a-module/greetings/greetings.go
@@ -3,12 +3,22 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Empty Name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
